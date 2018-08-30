c6ca78bf09880a292282e6c4d72309d15099b3b6
Chris Miller <chrisamiller@gmail.com>
2018-08-30 11:19:04 -0500

    handling edge case

--- mapq0_vcf_filter.sh
+++ mapq0_vcf_filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
 vcf=$1
 bam=$2
@@ -9,32 +9,41 @@ outvcf=$5
 
 
 #grab sites that don't already have the MQ0 field
+count=0;
 zgrep -v "^#" "$vcf" | grep -v "MQ0" | cut -f 1,2 | while read chr pos;do
     echo $chr $pos $((pos+1))
     pysamstats --type mapq --chromosome $chr --start $pos --end $((pos+1)) "$bam"  | grep $pos | cut -f 1,2,5 >>/tmp/mapq0counts
+    count=$((count+1))
 done
 
-#does the file contain the MQ0 field already?
-mqcount=0
-case "$vcf" in
-*.gz | *.tgz )
-    #gzipped vcf
-    mqcount=$(gunzip -c "$vcf" | grep "^#" | grep -w MQ0 | wc -l)
-        ;;
-*)
-    #non-gzipped vcf
-    mqcount=$(grep "^#" "$vcf" | grep -w MQ0 | wc -l)
-        ;;
-esac
-
-if [[ $mqcount -gt 0 ]];then
-    #already has mq0 set, we're all good
-    vcf-info-annotator --overwrite -o /tmp/mapq0.vcf "$vcf" /tmp/mapq0counts MQ0 
-else
-    #no mq0, need to set the header line as well
-    vcf-info-annotator -o /tmp/mapq0.vcf -f Integer -d "Number of MAPQ == 0 reads covering this record" "$vcf" /tmp/mapq0counts MQ0
+if [[ $count -eq 0 ]];then
+    #no sites to process, just copy the vcf
+    cp $vcf /tmp/mapq0.vcf
+else 
+    #need to add MQ0
+    
+    #does the file contain the MQ0 field already?
+    mqcount=0
+    case "$vcf" in
+        *.gz | *.tgz )
+            #gzipped vcf
+            mqcount=$(gunzip -c "$vcf" | grep "^#" | grep -w MQ0 | wc -l)
+            ;;
+        *)
+            #non-gzipped vcf
+            mqcount=$(grep "^#" "$vcf" | grep -w MQ0 | wc -l)
+            ;;
+    esac
+    
+    if [[ $mqcount -gt 0 ]];then
+        #already has mq0 set, we're all good
+        vcf-info-annotator --overwrite -o /tmp/mapq0.vcf "$vcf" /tmp/mapq0counts MQ0 
+    else
+        #no mq0, need to set the header line as well
+        vcf-info-annotator -o /tmp/mapq0.vcf -f Integer -d "Number of MAPQ == 0 reads covering this record" "$vcf" /tmp/mapq0counts MQ0
+    fi
 fi
- 
+
 #finally, set the filter tags on the vcf
 #the multiplication by 1.0 is necessary to convert integers to floats before dividing in the JEXL expression 
 #(which is dumb, and I want an hour of my life back)
